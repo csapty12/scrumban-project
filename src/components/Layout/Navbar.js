37dcf93485b5a58fe89709e91569a8f0e0a96e78
saptarshic <carlsaptarshi@rentalcars.com>
2018-11-08 16:59:24 +0000

    created and styled nav bar... not too happy with it, going to redesign it

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -4,7 +4,46 @@ import React, { Component } from "react";
 
 class Navbar extends Component {
   render() {
-    return <h1> Navbar will go here</h1>;
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+        <div className="container">
+          <a className="navbar-brand" href="Dashboard.html">
+            Scrumban Tool
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/dashboard">
+                  Dashboard
+                </a>
+              </li>
+            </ul>
+
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link " href="register.html">
+                  Sign Up
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="login.html">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
   }
 }
 export default Navbar;
