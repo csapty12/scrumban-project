4d9a0b552287de7b2f15c8f4ef9390e948ed0e8f
saptarshic <carlsaptarshi@rentalcars.com>
2018-11-08 08:44:14 +0000

    created react project, and inital welcome to your dashboard message

---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+/*this is where you can access all projects
+this is where you can click the form to create a new project.*/
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to your dashboard.</h1>
+      </div>
+    );
+  }
+}
+export default Dashboard;
