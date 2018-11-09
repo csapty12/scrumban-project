422f79b3c1241631714f91e82e43bf91d20e9693
saptarshic <carlsaptarshi@rentalcars.com>
2018-11-09 17:20:45 +0000

    json obect created which wil be sent to backend

--- client/src/components/Project/AddProject.js
+++ client/src/components/Project/AddProject.js
@@ -1,10 +1,109 @@
 import React, { Component } from "react";
 
+{
+  /* remember that class components can hold application state where as functional components can only hold property values*/
+}
 class AddProject extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      projectName: "",
+      projectIdentifier: "",
+      description: "",
+      startDate: "",
+      endDate: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const newProject = {
+      projectName: this.state.projectName,
+      projectIdentifier: this.state.projectIdentifier,
+      description: this.state.description,
+      startDate: this.state.startDate,
+      endDate: this.state.endDate
+    };
+    console.log(newProject);
+  }
+
   render() {
     return (
-      <div>
-        <h1> Add Project Form</h1>
+      <div className="project">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h5 className="display-4 text-center">Create / Edit Project</h5>
+              <hr />
+              <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-sm"
+                    placeholder="Project Name"
+                    name="projectName"
+                    value={this.state.projectName}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-sm"
+                    placeholder="Unique Project ID"
+                    name="projectIdentifier"
+                    value={this.state.projectIdentifier}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <textarea
+                    className="form-control form-control-sm"
+                    placeholder="Project Description"
+                    name="description"
+                    value={this.state.description}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <h6>Start Date</h6>
+                <div className="form-group">
+                  <input
+                    type="date"
+                    className="form-control form-control-sm"
+                    name="startDate"
+                    value={this.state.startDate}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <h6>Estimated End Date</h6>
+                <div className="form-group">
+                  <input
+                    type="date"
+                    className="form-control form-control-sm"
+                    name="endDate"
+                    value={this.state.endDate}
+                    onChange={this.handleChange}
+                  />
+                </div>
+
+                <input
+                  type="submit"
+                  className="btn btn-primary btn-block mt-4"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
