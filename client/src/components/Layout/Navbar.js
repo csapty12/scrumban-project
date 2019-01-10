f2766a3499235462834929229356d914ace28912
Carl Saptarshi <csapty12@gmail.com>
2019-01-10 00:34:13 +0000

    changing colour sheme

--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -11,7 +11,12 @@ import MenuIcon from "@material-ui/icons/Menu";
 
 const styles = theme => ({
   navbar: {
-    backgroundColor: "#007849"
+    background: "linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(33, 203, 243, .3)",
+    color: "white",
+    padding: "0 30px"
   },
   grow: {
     flexGrow: 1
