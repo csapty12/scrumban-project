f8897afaa40ede6f2b5e8075ecdd10f4f3ebf2f7
Carl Saptarshi <csapty12@gmail.com>
2019-03-08 19:00:30 +0000

    [ci] added in static content

---
+++ server/src/main/resources/static/precache-manifest.8d98e65df23de9db3540b1f4d6720e19.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "de13f5f7815b94fd2ddc",
+    "url": "/static/css/main.1c4c8564.chunk.css"
+  },
+  {
+    "revision": "de13f5f7815b94fd2ddc",
+    "url": "/static/js/main.de13f5f7.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cfd914c5eaff06f712e7",
+    "url": "/static/css/2.1996fd4d.chunk.css"
+  },
+  {
+    "revision": "cfd914c5eaff06f712e7",
+    "url": "/static/js/2.cfd914c5.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f7d30101a1bbb6df43aac1d5f8ea3f72",
+    "url": "/index.html"
+  }
+];
