5cb2460acc32a603431978b5b0db499eb8dd3253
Carl Saptarshi <csapty12@gmail.com>
2019-03-08 19:07:10 +0000

    [js] working on static links

--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -6,6 +6,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import Link from "react-router-dom";
 
 const styles = theme => ({
   navbar: {
@@ -30,9 +31,14 @@ class Navbar extends Component {
             <Typography variant="h6" color="inherit" className={classes.grow}>
               TrellBan
             </Typography>
-            <Button color="inherit" href="/dashboard">
-              Dashboard
-            </Button>
+            {
+              <Link to="/dashboard">
+                <Button color="inherit">Dashboard</Button>
+              </Link>
+              //   <Button color="inherit" href="/dashboard">
+              //   Dashboard
+              // </Button>
+            }
             <Button color="inherit">Login</Button>
             <Button color="inherit">Register</Button>
           </Toolbar>
